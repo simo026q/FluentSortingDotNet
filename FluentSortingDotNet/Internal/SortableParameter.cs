bf0398ad9868d5f40986f448463f5788a2e2e658
Simon Bøye Christensen <sbc@scantalk.com>
2024-11-18 12:06:51 +0100

    Fix default name of SortableParameter

--- FluentSortingDotNet/Internal/SortableParameter.cs
+++ FluentSortingDotNet/Internal/SortableParameter.cs
@@ -7,6 +7,16 @@ internal sealed class SortableParameter<T, TProperty>(Expression<Func<T, TProper
 {
     public LambdaExpression Expression { get; } = expression;
     public bool IsRequired { get; set; }
-    public string Name { get; set; } = expression.Body.ToString();
+    public string Name { get; set; } = GetNameFromExpression(expression);
     public SortDirection? DefaultDirection { get; set; }
+
+    private static string GetNameFromExpression(Expression<Func<T, TProperty>> expression)
+    {
+        var bodyString = expression.Body.ToString();
+        var prefix = expression.Parameters[0].Name + ".";
+
+        return bodyString.StartsWith(prefix) 
+            ? bodyString.Substring(prefix.Length) 
+            : bodyString;
+    }
 }
