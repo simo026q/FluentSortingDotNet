003788dfa6f82cba442044f4c0aa66a84aacffdc
Simon Bøye Christensen <sbc@scantalk.com>
2024-12-03 13:41:26 +0100

    Add QueryableMethods static class

--- src/FluentSortingDotNet/Internal/QueryableExtensions.cs
+++ src/FluentSortingDotNet/Internal/QueryableExtensions.cs
@@ -1,27 +1,11 @@
 ﻿using System.Linq.Expressions;
 using System.Linq;
 using System.Reflection;
-using FluentSortingDotNet.Internal;
 
 namespace FluentSortingDotNet.Internal;
 
 internal static class QueryableExtensions
 {
-    private static readonly MethodInfo OrderByMethod;
-    private static readonly MethodInfo OrderByDescendingMethod;
-    private static readonly MethodInfo ThenByMethod;
-    private static readonly MethodInfo ThenByDescendingMethod;
-
-    static QueryableExtensions()
-    {
-        MethodInfo[] methods = typeof(Queryable).GetMethods(BindingFlags.Static | BindingFlags.Public);
-
-        OrderByMethod = methods.First(x => x.Name == nameof(Queryable.OrderBy) && x.GetParameters().Length == 2);
-        OrderByDescendingMethod = methods.First(x => x.Name == nameof(Queryable.OrderByDescending) && x.GetParameters().Length == 2);
-        ThenByMethod = methods.First(x => x.Name == nameof(Queryable.ThenBy) && x.GetParameters().Length == 2);
-        ThenByDescendingMethod = methods.First(x => x.Name == nameof(Queryable.ThenByDescending) && x.GetParameters().Length == 2);
-    }
-
     private static IOrderedQueryable<T> CreateOrderQuery<T>(IQueryable<T> query, LambdaExpression expression, MethodInfo method)
     {
         return (IOrderedQueryable<T>)query.Provider.CreateQuery<T>(Expression.Call(
@@ -32,14 +16,14 @@ internal static class QueryableExtensions
     }
 
     public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> queryable, LambdaExpression expression)
-        => CreateOrderQuery(queryable, expression, OrderByMethod);
+        => CreateOrderQuery(queryable, expression, QueryableMethods.OrderBy);
 
     public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> queryable, LambdaExpression expression)
-        => CreateOrderQuery(queryable, expression, OrderByDescendingMethod);
+        => CreateOrderQuery(queryable, expression, QueryableMethods.OrderByDescending);
 
     public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> queryable, LambdaExpression expression)
-        => CreateOrderQuery(queryable, expression, ThenByMethod);
+        => CreateOrderQuery(queryable, expression, QueryableMethods.ThenBy);
 
     public static IOrderedQueryable<T> ThenByDescending<T>(this IOrderedQueryable<T> queryable, LambdaExpression expression)
-        => CreateOrderQuery(queryable, expression, ThenByDescendingMethod);
+        => CreateOrderQuery(queryable, expression, QueryableMethods.ThenByDescending);
 }
---
+++ src/FluentSortingDotNet/Internal/QueryableMethods.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Reflection;
+using FluentSortingDotNet.Internal;
+
+namespace FluentSortingDotNet.Internal;
+
+internal static class QueryableMethods
+{
+    public static readonly MethodInfo OrderBy;
+    public static readonly MethodInfo OrderByDescending;
+    public static readonly MethodInfo ThenBy;
+    public static readonly MethodInfo ThenByDescending;
+
+    static QueryableMethods()
+    {
+        var methods = typeof(Queryable)
+            .GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.DeclaredOnly)
+            .GroupBy(x => x.Name)
+            .ToDictionary(x => x.Key, x => x.ToList());
+
+        OrderBy = GetMethod(nameof(Queryable.OrderBy), 2);
+        OrderByDescending = GetMethod(nameof(Queryable.OrderByDescending), 2);
+        ThenBy = GetMethod(nameof(Queryable.ThenBy), 2);
+        ThenByDescending = GetMethod(nameof(Queryable.ThenByDescending), 2);
+
+        MethodInfo GetMethod(string name, int parameterCount)
+        {
+            return methods[name].First(x => x.GetParameters().Length == parameterCount);
+        }
+    }
+}
