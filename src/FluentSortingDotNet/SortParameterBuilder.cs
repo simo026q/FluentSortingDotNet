b9d64ea7852dc8871b0317147826685f46e9898d
Simon Bøye Christensen <sbc@scantalk.com>
2025-04-16 16:39:43 +0200

    Remove deprecated methods in SortParameterBuilder

--- src/FluentSortingDotNet/SortParameterBuilder.cs
+++ src/FluentSortingDotNet/SortParameterBuilder.cs
@@ -1,5 +1,4 @@
 ﻿using FluentSortingDotNet.Internal;
-using System;
 
 namespace FluentSortingDotNet;
 
@@ -18,15 +17,6 @@ public sealed class SortParameterBuilder
     /// <summary>
     /// Set the sort parameter as a default parameter when the sort query is empty.
     /// </summary>
-    /// <param name="sortDirection">The sort direction of the default parameter.</param>
-    /// <returns>The current builder instance.</returns>
-    [Obsolete("Use IsDefault instead. This method will be removed in the next major or minor version.", error: true)]
-    public SortParameterBuilder Default(SortDirection sortDirection) 
-        => IsDefault(sortDirection);
-
-    /// <summary>
-    /// Set the sort parameter as a default parameter when the sort query is empty.
-    /// </summary>
     /// <param name="direction">The sort direction of the default parameter.</param>
     /// <returns>The current builder instance.</returns>
     public SortParameterBuilder IsDefault(SortDirection direction)
@@ -34,15 +24,6 @@ public sealed class SortParameterBuilder
         _parameter.DefaultDirection = direction;
         return this;
     }
-
-    /// <summary>
-    /// Specifies a custom name of the parameter.
-    /// </summary>
-    /// <param name="name">The name of the parameter.</param>
-    /// <returns>The current builder instance.</returns>
-    [Obsolete("Use WithName instead. This method will be removed in the next major or minor version.", error: true)]
-    public SortParameterBuilder Name(string name) 
-        => WithName(name);
 
     /// <summary>
     /// Set the name of the sort parameter. By default, the name is inferred from the property expression.
